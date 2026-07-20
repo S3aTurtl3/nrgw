0bb862443daa78d0088a4f1c595fb5d63f45b7e6
Ore <orealao@mit.edu>
2026-07-19 20:32:01 -0400

    real t

---
+++ scripts/hypertun.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --account=iaifi_lab
+#SBATCH -J ice # Job name
+#SBATCH -p iaifi_gpu # Partition(s) (separate with
+# commas if using multiple)
+#SBATCH --ntasks=1 # Number of cores
+#SBATCH --gpus=1
+#SBATCH -t 0-03:00:00 # Time (D-HH:MM:SS)
+#SBATCH --mem=80G # Memory
+#SBATCH -o icepy_%j.o # Name of standard output file
+#SBATCH -e icepy_%j.e # Name of standard error file
+#SBATCH --signal=USR1@4
+#SBATCH --signal=USR1@2
+#SBATCH --mail-user=orealao@mit.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+# FINally get email notifications
+
+# first use version of script with only 2 trials and dataset size 100, ten increase requested time
+# pixi add wandb for final
+module load Miniforge3/26.1.0-fasrc01
+
+cd  /n/holystore01/LABS/iaifi_lab/Users/oalao/nrgw
+
+# Force offline mode for W&B
+export WANDB_MODE=offline
+
+
+pixi run -e gpu python src/hypertune1D/scriptt.py --temp=10 --batch_size=500 --steps=20000  --num_trials=20 --num_train_samples=19000 --num_test_samples=2500 --num_val_samples=2500 --lattice_size=32 --out="/n/holystore01/LABS/iaifi_lab/Users/oalao/runstuff" --dir_model_weights="/scratch"

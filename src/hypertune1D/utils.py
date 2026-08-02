b4febd7009cd868867432c92b3f9ccbaec409726
Ore <orealao@mit.edu>
2026-08-02 02:44:59 -0400

    f

--- src/hypertune1D/utils.py
+++ src/hypertune1D/utils.py
@@ -1,3 +1,4 @@
+import hashlib
 def get_unique_identifier(info: dict):
     """Returns a string, short enough to be part of a filename, such that get_unique_identifier(a) == get_unique_identifier(b) if and only if a and b have the same key:value pairs"""
     unique_string = str(list(info.items()).sort())
